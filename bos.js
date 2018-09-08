7c25c395751e5d06cf53df2c946c1b89e7d97879
Omertis <36013948+Omertis@users.noreply.github.com>
2018-09-08 21:28:48 +0300

    Update bos.js

--- bos.js
+++ bos.js
@@ -7,8 +7,6 @@ client.on('ready', () => {
 
 
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const ytdl = require('ytdl-core');
 const request = require('request');
 const fs = require('fs');
@@ -16,7 +14,6 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
  
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '.';
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
 });
